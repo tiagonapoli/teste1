66047e77f6bb8c7a7d0a022b8bd083eacaa3dc56
tiagonapoli <tiago.napoli@vtex.com.br>
2020-01-15 13:59:23 -0300

    wip

--- src/lib/CustomCommand.ts
+++
@@ -1,8 +0,0 @@
-import OclifCommand from '@oclif/command'
-import { ParsingToken } from '@oclif/parser/lib/parse'
-
-export abstract class CustomCommand extends OclifCommand {
-  getAllArgs(rawParse: ParsingToken[]) {
-    return rawParse.filter(token => token.type === 'arg').map(token => token.input)
-  }
-}
